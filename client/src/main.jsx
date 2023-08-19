c56dca45877cb53d9e3ac864b082f2b3a099757e
Ivan <carivan14@homtail.com>
2023-08-18 22:49:34 -0300

    Deploy

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,10 @@ import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import axios from 'axios'
 import store from './redux/store/store'
+
+axios.defaults.baseURL = 'https://pi-back-production-e9b7.up.railway.app/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
